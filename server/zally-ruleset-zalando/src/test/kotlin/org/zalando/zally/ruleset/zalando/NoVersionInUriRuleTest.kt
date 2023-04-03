5ab4f055a5105374bdc0ac9fefc5aaa47892e8a0
Peter Cadman <pcadman@linz.govt.nz>
2023-04-04 11:43:45 +1200

    chore: Add Tests for server and no version rule

--- server/zally-ruleset-zalando/src/test/kotlin/org/zalando/zally/ruleset/zalando/NoVersionInUriRuleTest.kt
+++ server/zally-ruleset-zalando/src/test/kotlin/org/zalando/zally/ruleset/zalando/NoVersionInUriRuleTest.kt
@@ -4,6 +4,7 @@ import org.zalando.zally.core.DefaultContextFactory
 import org.assertj.core.api.Assertions.assertThat
 import org.intellij.lang.annotations.Language
 import org.junit.jupiter.api.Test
+import org.zalando.zally.ruleset.zalando.model.ApiAudience
 
 class NoVersionInUriRuleTest {
 
@@ -58,5 +59,69 @@ class NoVersionInUriRuleTest {
         val violations = rule.checkServerURLs(context)
 
         assertThat(violations).isEmpty()
+    }
+
+    @Test
+    fun `Expect violations When Component internal and missing version`() {
+        @Language("YAML")
+        val spec = """
+        openapi: 3.0.0
+        info:
+          x-audience: ${ApiAudience.COMPANY_INTERNAL}
+        servers:
+          - url: "https://api.landonline.govt.nz/anything"
+          - url: "https://api{env}.landonline.govt.nz/anything"               
+          
+        """.trimIndent()
+        val context = DefaultContextFactory().getOpenApiContext(spec)
+
+        val violations = rule.validate(context)
+
+        //assertEquals(2, violations.size)
+        //assertEquals("url doesn't pass regex", violations[0].description)
+        //assertEquals("/servers/0/url", violations[0].pointer.toString())
+    }
+
+    @Test
+    fun `Expect violations When Component internal and version alphanumeric`() {
+        @Language("YAML")
+        val spec = """
+        openapi: 3.0.0
+        info:
+          x-audience: ${ApiAudience.COMPANY_INTERNAL}
+        servers:
+          - url: "https://api.landonline.govt.nz/v1a"
+          - url: "https://api{env}.landonline.govt.nz/v1a"               
+          
+        """.trimIndent()
+        val context = DefaultContextFactory().getOpenApiContext(spec)
+
+        val violations = rule.validate(context)
+
+        //assertEquals(2, violations.size)
+        //assertEquals("url doesn't pass regex", violations[0].description)
+        //assertEquals("/servers/0/url", violations[0].pointer.toString())
+    }
+
+
+    @Test
+    fun `Expect violations When Component internal and version too long`() {
+        @Language("YAML")
+        val spec = """
+        openapi: 3.0.0
+        info:
+          x-audience: ${ApiAudience.COMPANY_INTERNAL}
+        servers:
+          - url: "https://api.landonline.govt.nz/v123/anything"
+          - url: "https://api{env}.landonline.govt.nz/v123/anything"               
+          
+        """.trimIndent()
+        val context = DefaultContextFactory().getOpenApiContext(spec)
+
+        val violations = rule.validate(context)
+
+        //assertEquals(2, violations.size)
+        //assertEquals("url doesn't pass regex", violations[0].description)
+        //assertEquals("/servers/0/url", violations[0].pointer.toString())
     }
 }
--- server/zally-ruleset-zalando/src/test/kotlin/org/zalando/zally/ruleset/zalando/ServerRuleTest.kt
+++ server/zally-ruleset-zalando/src/test/kotlin/org/zalando/zally/ruleset/zalando/ServerRuleTest.kt
@@ -8,23 +8,282 @@ import org.zalando.zally.test.ZallyAssertions.assertThat
 import org.zalando.zally.core.DefaultContextFactory
 import org.zalando.zally.rule.api.Context
 import org.junit.jupiter.api.Test
+import org.zalando.zally.ruleset.zalando.model.ApiAudience
 
 class ServerRuleTest {
 
     private val rule = ServersRule(rulesConfig)
 
 
-
     @Test
-    fun `correct servers are correctly identified`() {
+    fun `Expect no violations When Company internal with api landonline servers`() {
         @Language("YAML")
         val spec = """
         openapi: 3.0.0
         info:
-          x-audience: "company-internal"
+          x-audience: ${ApiAudience.COMPANY_INTERNAL}
         servers:
           - url: "https://api.landonline.govt.nz/v22/anything"
           - url: "https://api{env}.landonline.govt.nz/v22/anything"               
+          
+        """.trimIndent()
+        val context = DefaultContextFactory().getOpenApiContext(spec)
+
+        val violations = rule.validate(context)
+
+        //assertEquals(2, violations.size)
+        //assertEquals("url doesn't pass regex", violations[0].description)
+        //assertEquals("/servers/0/url", violations[0].pointer.toString())
+    }
+
+    @Test
+    fun `Expect violations When Company internal and mixed api and public servers`() {
+        @Language("YAML")
+        val spec = """
+        openapi: 3.0.0
+        info:
+          x-audience: ${ApiAudience.COMPANY_INTERNAL}
+        servers:
+          - url: "https://api.landonline.govt.nz/v22/anything"
+          - url: "https://public.api{env}.landonline.govt.nz/v22/anything"               
+          
+        """.trimIndent()
+        val context = DefaultContextFactory().getOpenApiContext(spec)
+
+        val violations = rule.validate(context)
+
+        //assertEquals(2, violations.size)
+        //assertEquals("url doesn't pass regex", violations[0].description)
+        //assertEquals("/servers/0/url", violations[0].pointer.toString())
+    }
+
+    @Test
+    fun `Expect no violations When Component internal with api landonline servers`() {
+        @Language("YAML")
+        val spec = """
+        openapi: 3.0.0
+        info:
+          x-audience: ${ApiAudience.COMPONENT_INTERNAL}
+        servers:
+          - url: "https://api.landonline.govt.nz/v22/anything"
+          - url: "https://api{env}.landonline.govt.nz/v22/anything"               
+          
+        """.trimIndent()
+        val context = DefaultContextFactory().getOpenApiContext(spec)
+
+        val violations = rule.validate(context)
+
+        //assertEquals(2, violations.size)
+        //assertEquals("url doesn't pass regex", violations[0].description)
+        //assertEquals("/servers/0/url", violations[0].pointer.toString())
+    }
+
+    @Test
+    fun `Expect violations When Component internal and mixed api and public servers`() {
+        @Language("YAML")
+        val spec = """
+        openapi: 3.0.0
+        info:
+          x-audience: ${ApiAudience.COMPANY_INTERNAL}
+        servers:
+          - url: "https://api.landonline.govt.nz/v22/anything"
+          - url: "https://public.api{env}.landonline.govt.nz/v22/anything"               
+          
+        """.trimIndent()
+        val context = DefaultContextFactory().getOpenApiContext(spec)
+
+        val violations = rule.validate(context)
+
+        //assertEquals(2, violations.size)
+        //assertEquals("url doesn't pass regex", violations[0].description)
+        //assertEquals("/servers/0/url", violations[0].pointer.toString())
+    }
+
+    @Test
+    fun `Expect no violations When External Public with public api landonline servers`() {
+        @Language("YAML")
+        val spec = """
+        openapi: 3.0.0
+        info:
+          x-audience: ${ApiAudience.EXTERNAL_PUBLIC}
+        servers:
+          - url: "https://public.api.landonline.govt.nz/v22/anything"
+          - url: "https://public.api{env}.landonline.govt.nz/v22/anything"               
+          
+        """.trimIndent()
+        val context = DefaultContextFactory().getOpenApiContext(spec)
+
+        val violations = rule.validate(context)
+
+        //assertEquals(2, violations.size)
+        //assertEquals("url doesn't pass regex", violations[0].description)
+        //assertEquals("/servers/0/url", violations[0].pointer.toString())
+    }
+
+    @Test
+    fun `Expect violations When External Public and mixed api and public servers`() {
+        @Language("YAML")
+        val spec = """
+        openapi: 3.0.0
+        info:
+          x-audience: ${ApiAudience.COMPANY_INTERNAL}
+        servers:
+          - url: "https://api.landonline.govt.nz/v22/anything"
+          - url: "https://public.api{env}.landonline.govt.nz/v22/anything"               
+          
+        """.trimIndent()
+        val context = DefaultContextFactory().getOpenApiContext(spec)
+
+        val violations = rule.validate(context)
+
+        //assertEquals(2, violations.size)
+        //assertEquals("url doesn't pass regex", violations[0].description)
+        //assertEquals("/servers/0/url", violations[0].pointer.toString())
+    }
+
+    @Test
+    fun `Expect violations When External Public and api servers`() {
+        @Language("YAML")
+        val spec = """
+        openapi: 3.0.0
+        info:
+          x-audience: ${ApiAudience.EXTERNAL_PUBLIC}
+        servers:
+          - url: "https://api.landonline.govt.nz/v22/anything"
+          - url: "https://api{env}.landonline.govt.nz/v22/anything"               
+          
+        """.trimIndent()
+        val context = DefaultContextFactory().getOpenApiContext(spec)
+
+        val violations = rule.validate(context)
+
+        //assertEquals(2, violations.size)
+        //assertEquals("url doesn't pass regex", violations[0].description)
+        //assertEquals("/servers/0/url", violations[0].pointer.toString())
+    }
+
+    @Test
+    fun `Expect violations When Company internal and public api servers`() {
+        @Language("YAML")
+        val spec = """
+        openapi: 3.0.0
+        info:
+          x-audience: ${ApiAudience.COMPANY_INTERNAL}
+        servers:
+          - url: "https://public.api.landonline.govt.nz/v22/anything"
+          - url: "https://public.api{env}.landonline.govt.nz/v22/anything"               
+          
+        """.trimIndent()
+        val context = DefaultContextFactory().getOpenApiContext(spec)
+
+        val violations = rule.validate(context)
+
+        //assertEquals(2, violations.size)
+        //assertEquals("url doesn't pass regex", violations[0].description)
+        //assertEquals("/servers/0/url", violations[0].pointer.toString())
+    }
+
+    @Test
+    fun `Expect violations When Component internal and public api servers`() {
+        @Language("YAML")
+        val spec = """
+        openapi: 3.0.0
+        info:
+          x-audience: ${ApiAudience.COMPONENT_INTERNAL}
+        servers:
+          - url: "https://public.api.landonline.govt.nz/v22/anything"
+          - url: "https://public.api{env}.landonline.govt.nz/v22/anything"               
+          
+        """.trimIndent()
+        val context = DefaultContextFactory().getOpenApiContext(spec)
+
+        val violations = rule.validate(context)
+
+        //assertEquals(2, violations.size)
+        //assertEquals("url doesn't pass regex", violations[0].description)
+        //assertEquals("/servers/0/url", violations[0].pointer.toString())
+    }
+
+    @Test
+    fun `Expect violations When Component internal and basePath missing`() {
+        @Language("YAML")
+        val spec = """
+        openapi: 3.0.0
+        info:
+          x-audience: ${ApiAudience.COMPANY_INTERNAL}
+        servers:
+          - url: "https://api.landonline.govt.nz/v12"
+          - url: "https://api{env}.landonline.govt.nz/v12"               
+          
+        """.trimIndent()
+        val context = DefaultContextFactory().getOpenApiContext(spec)
+
+        val violations = rule.validate(context)
+
+        //assertEquals(2, violations.size)
+        //assertEquals("url doesn't pass regex", violations[0].description)
+        //assertEquals("/servers/0/url", violations[0].pointer.toString())
+    }
+
+    @Test
+    fun `Expect violations When Component internal and basePath contains two resources `() {
+        @Language("YAML")
+        val spec = """
+        openapi: 3.0.0
+        info:
+          x-audience: ${ApiAudience.COMPANY_INTERNAL}
+        servers:
+          - url: "https://api.landonline.govt.nz/v12/myresource/subresource"
+          - url: "https://api{env}.landonline.govt.nz/v12/myresource/subresource"               
+          
+        """.trimIndent()
+        val context = DefaultContextFactory().getOpenApiContext(spec)
+
+        val violations = rule.validate(context)
+
+        //assertEquals(2, violations.size)
+        //assertEquals("url doesn't pass regex", violations[0].description)
+        //assertEquals("/servers/0/url", violations[0].pointer.toString())
+    }
+
+    @Test
+    fun `Expect violations When Component internal there exists no template variable called env`() {
+        @Language("YAML")
+        val spec = """
+        openapi: 3.0.0
+        info:
+          x-audience: ${ApiAudience.COMPANY_INTERNAL}
+        servers:
+          - url: "https://api.landonline.govt.nz/v12/myresource"
+          - url: "https://api.landonline.govt.nz/v12/myresource"
+          
+        """.trimIndent()
+        val context = DefaultContextFactory().getOpenApiContext(spec)
+
+        val violations = rule.validate(context)
+
+        //assertEquals(2, violations.size)
+        //assertEquals("url doesn't pass regex", violations[0].description)
+        //assertEquals("/servers/0/url", violations[0].pointer.toString())
+    }
+
+    @Test
+    fun `Expect violations When variable env not declared`() {
+        @Language("YAML")
+        val spec = """
+        openapi: 3.0.0
+        info:
+          x-audience: ${ApiAudience.COMPANY_INTERNAL}
+        servers:
+          - url: "https://api.landonline.govt.nz/v12/myresource"
+          - url: "https://api{env}.landonline.govt.nz/v12/myresource"
+        variables:
+          env:
+            enum:
+              - .dev
+              - .env
+            default: .dev
+            description: Not prod environments
           
         """.trimIndent()
         val context = DefaultContextFactory().getOpenApiContext(spec)
